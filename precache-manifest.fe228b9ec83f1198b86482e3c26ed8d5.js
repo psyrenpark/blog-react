07f03ab770ae7b1b77f6f37a0e49b10d34626022
경복박 <gyeongbogbag@gyeongbogbags-MacBook-Pro.local>
2020-03-01 15:10:52 +0900

    Updates

---
+++ precache-manifest.fe228b9ec83f1198b86482e3c26ed8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b133f4b35eb6af0a642e2dba9f7ab100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ef44b1d804e6c944057",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58fadd8d3f8605357ddb",
+    "url": "/static/js/2.d6c17b04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d6c17b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef44b1d804e6c944057",
+    "url": "/static/js/main.0951de46.chunk.js"
+  },
+  {
+    "revision": "f34df4cc19e0da90cba3",
+    "url": "/static/js/runtime-main.578b6095.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
