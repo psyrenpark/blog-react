668617da04e954c2d5a9612860606942ca50cf86
경복박 <gyeongbogbag@gyeongbogbags-MacBook-Pro.local>
2020-03-01 17:22:59 +0900

    ## blog fix3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  useLocation
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>test</h2>
+      <h2>test22</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+
+function Test(){
+
+  const basename = "/";
+
+  return (
+    <Router basename={basename}>
+    <App />
+  </Router>
+  )
+}
+
+
+export default Test;
