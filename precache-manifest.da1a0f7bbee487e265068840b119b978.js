31805b82202e633ba237cc0090804cae5e623992
경복박 <gyeongbogbag@gyeongbogbags-MacBook-Pro.local>
2020-03-01 15:34:55 +0900

    Updates

--- precache-manifest.da1a0f7bbee487e265068840b119b978.js
+++ precache-manifest.da1a0f7bbee487e265068840b119b978.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "375f96d9475c78ae49827888313c33ed",
+    "revision": "2f0c84d3abaf870e29532dcbca2339fd",
     "url": "/blog-react/index.html"
   },
   {
-    "revision": "c7b5767b6c40d8bc8109",
+    "revision": "93af9a5c406301d4fb34",
     "url": "/blog-react/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "c6bab1e17d92541e7644",
-    "url": "/blog-react/static/js/2.c39c0853.chunk.js"
+    "revision": "1bae5c9d5bd13c700591",
+    "url": "/blog-react/static/js/2.dcb7eeb1.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/blog-react/static/js/2.c39c0853.chunk.js.LICENSE.txt"
+    "url": "/blog-react/static/js/2.dcb7eeb1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c7b5767b6c40d8bc8109",
-    "url": "/blog-react/static/js/main.d7a54625.chunk.js"
+    "revision": "93af9a5c406301d4fb34",
+    "url": "/blog-react/static/js/main.5c8a7c40.chunk.js"
   },
   {
     "revision": "e1b750370b98cefde77e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog-react/precache-manifest.689d42d29a22da2ef28386e9f51c8bbb.js"
+  "/blog-react/precache-manifest.da1a0f7bbee487e265068840b119b978.js"
 );
 
 self.addEventListener('message', (event) => {
